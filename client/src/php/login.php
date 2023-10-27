673ba897bf9bc3aa9f7a86cca1da62e298bee06e
Paulo Siewert <borraxa.hacher@gmail.com>
2023-10-27 12:16:53 -0300

    positivo e operante

--- client/src/php/login.php
+++ client/src/php/login.php
@@ -3,33 +3,44 @@
 
 <head>
   <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Ficha de Ocorrência</title>
+
+  <!-- Demais folders -->
   <link rel="icon" href="/client/src/assets/noarlogo.png" />
   <link rel="stylesheet" href="/client/src/css/index.css" />
   <link rel="stylesheet" href="/client/src/css/reset.css" />
-  <link rel="stylesheet" href="/client/src/javascript/sample.js" />
 
+  <!--CSS and Font Awesome, Bootstrap, etc. links -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
-  <!-- Bootstrap JS and Popper.js -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <!--CSS and Font Awesome, Bootstrap, etc. links -->
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
     crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
+    integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="/client/src/javascript/index.js"></script>
+
 </head>
 
 <body>
   <!-- COMEÇO DA HEADER -->
 
-  <div class="container-fluid m-0 p-0">
+  <div class="container-fluid m-0 p-0" id="element-to-hide">
     <header class="fixed-top">
       <nav class="navbar navbar-dark bg-dark">
         <div class="container">
+
           <div class="header__container">
             <b class="header__title">Bombeiros Voluntários</b>
             <p class="header__title">
@@ -40,6 +51,7 @@
           <img src="/client/src/assets/noarlogo (1).png" alt="logo" />
         </div>
       </nav>
+
     </header>
   </div>
 
@@ -54,39 +66,44 @@
 
         <!-- USUÁRIO -->
 
-        <div class="col-lg-12">
-          <label for="numero" class="form-label m-0">CPF:</label>
-          <input type="text" name="password" required oninput="mascara(this)" class="form-control" id="numero" aria-describedby="emailHelp" />
-        </div>
+        <form>
 
-        <!-- FIM USUÁRIO -->
+          <div class="col-lg-12">
+            <label for="numero" class="form-label m-0">CPF:</label>
+            <input type="text" name="password" required oninput="mascara(this)" class="form-control" id="numero"
+              aria-describedby="emailHelp" />
+          </div>
 
-        <br />
-        <br />
-        <br />
-        <br />
+          <!-- FIM USUÁRIO -->
 
-        <!-- SENHA -->
+          <br />
+          <br />
+          <br />
+          <br />
 
-        <div class="col-lg-12 mt-3">
-          <label for="numero" class="form-label m-0">Senha: </label>
+          <!-- SENHA -->
 
-
-          <input type="password" name="password" required class="form-control" id="numero" />
-        </div>
-
-        <!-- FIM SENHA -->
+          <div class="col-lg-12 mt-3">
+            <label for="numero" class="form-label m-0">Senha: </label>
 
 
-        <!-- Login -->
+            <input type="password" name="password" required class="form-control" id="numero" />
+          </div>
 
-        <div class="container mt-5">
-          <div class="row">
-            <div class="col-10">
-              <input type="submit" value="Login" class="fw-3 fw-bold d-flex btn btn-primary w-100 justify-content-center"/>
+          <!-- FIM SENHA -->
+
+
+          <!-- Login -->
+
+          <div class="container mt-5">
+            <div class="row justify-content-center">
+              <div class="col-6 ">
+                <input type="submit" value="Login" class="fw-bold bnt-block btn btn-dark w-100" />
+              </div>
             </div>
           </div>
-        </div>
+
+        </form>
 
         <!-- <input type="submit" value="Login"> -->
 
